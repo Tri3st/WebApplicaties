d601ee9d998c190b406a7b669ba425a9ee6c228d
Martin van Diest <martin.van.diest@intemo.com>
2023-03-06 09:21:21 +0100

    Paar dingen veranderd.
    Krijg nu beginscherm te zien.

--- martin/js/snake.js
+++ martin/js/snake.js
@@ -32,24 +32,25 @@ $(document).ready(function() {
 });
 
 /**
-  @function init() -> void
-  @desc Haal eventueel bestaand voedsel en een bestaande slang weg, cre\"eer een slang, genereer voedsel, en teken alles
+ * @function init()
+ * @description Haal eventueel bestaand voedsel en een bestaande slang weg, cre\"eer een slang,
+ *       genereer voedsel, en teken alles
 */
 function init() {
     width = $('#mySnakeCanvas')[0].width;
     height = $('#mySnakeCanvas')[0].height;
     xMax = width - R;
     yMax = height - R;
-	createStartSnake();
-    createFoods();
+	snake = createStartSnake();
+    foods = createFoods();
     draw();
     // startTimer();
 }
 
 /**
-  @function move(direction) -> void
-  @desc Beweeg slang in aangegeven richting
-        tenzij slang uit canvas zou verdwijnen  
+  @function move(direction)
+  @description Beweeg slang in aangegeven richting
+               tenzij slang uit canvas zou verdwijnen
   @param   {string} direction de richting (een van de constanten UP, DOWN, LEFT of RIGHT)
 */
 function move(direction) {
@@ -63,8 +64,8 @@ function move(direction) {
 }
 
 /**
-  @function draw() -> void
-  @desc Teken de slang en het voedsel
+  @function draw()
+  @description Teken de slang en het voedsel
 */
 function draw() {
     var myCanvas = $('#mySnakeCanvas').get(0);
@@ -83,6 +84,9 @@ function draw() {
  ***************************************************************************/
 /**
    @constructor Snake
+   @description Klasse Snake met 2 functies:
+                    - canMove : controlleer of slang niet van het canvas af loopt.
+                    - doMove  : doe de beweging
    @param {[Element]} segments een array met aaneengesloten slangsegmenten
                    Het laatste element van segments wordt de kop van de slang 
 */ 
@@ -92,23 +96,37 @@ function Snake(segments) {
     this.head.color = HEAD;
     this.tail = segments[0];
     this.tail.color = SNAKE;
-    this.direction = UP;
+    this.direction = direction;
     this.canMove = function(direction) {
         var head = this.head;
         switch (direction) {
-            case UP:    return head.y > YMIN;
-            case DOWN:  return head.y < yMax;
-            case LEFT:  return head.x > XMIN;
-            case RIGHT: return head.x < xMax;
+            case UP:
+                return head.y > YMIN;
+            case DOWN:
+                return head.y < yMax;
+            case LEFT:
+                return head.x > XMIN;
+            case RIGHT:
+                return head.x < xMax;
         }
     }
     this.doMove = function(direction) {
         var head = this.head;
         switch (direction) {
-            case UP:    head.y -= STEP; break;
-            case DOWN:  head.y += STEP; break;
-            case LEFT:  head.x -= STEP; break;
-            case RIGHT: head.x += STEP; break;
+            case UP:
+                head.y -= STEP;
+                break;
+            case DOWN:
+                head.y += STEP;
+                break;
+            case LEFT:
+                head.x -= STEP;
+                break;
+            case RIGHT:
+                head.x += STEP;
+                break;
+            default:
+                break;
         }
         this.segments.push(head);
         this.head = this.segments.pop();
@@ -122,11 +140,15 @@ function Snake(segments) {
 }
 
 /**
-   @constructor Element
-   @param radius straal
-   @param {number} x x-coordinaat middelpunt
-   @param {number} y y-coordinaat middelpunt
-   @param {string} color kleur van het element
+ * @constructor Element
+ * @description Klasse Element (segment van slang of food) met 1 functie:
+ *              - collidesWithOneOf : controlleert of het element met één van de
+ *                                    meegegeven elementen botst.
+ *
+ * @param {number} radius straal
+ * @param {number} x x-coordinaat middelpunt
+ * @param {number} y y-coordinaat middelpunt
+ * @param {string} color kleur van het element
 */ 
 function Element(radius, x, y, color) {
     this.radius = radius;
@@ -136,7 +158,7 @@ function Element(radius, x, y, color) {
     this.collidesWithOneOf = function(elements) {
         elements.forEach((element) => {
             // TODO implement
-            if (element === true) {
+            if (Math.abs(this.x - element.x) >= 2 * R || Math.abs(this.y - element.y) >= 2 * R) {
                 return true;
             }
         })
@@ -149,14 +171,14 @@ function Element(radius, x, y, color) {
  
 /**
   @function createStartSnake() -> Snake
-  @desc Slang creëren, bestaande uit  twee segmenten, 
-        in het midden van het veld
+  @description Slang creëren, bestaande uit  twee segmenten,
+               in het midden van het veld
   @return: slang volgens specificaties
 */
 function createStartSnake() {
 	var segments   = [createSegment(R + width/2, R + height/2), 
 	                  createSegment(R + width/2, height/2 - R)];
-    snake = new Snake(segments);  
+     return new Snake(segments);
 }
 /**
   @function createSegment(x,y) -> Element
@@ -185,18 +207,18 @@ function createFood(x, y) {
   @param  {dom object} canvas het tekenveld
 */
  function drawElement(element, context) {
-	context.drawArc({
-		draggable : false,
-		fillStyle : element.color,
-		x : element.x,
-		y : element.y,
-		radius : element.radius
-	});
+     const x = element.x;
+     const y = element.y;
+     const radius = element.radius;
+     context.fillStyle = element.color;
+     context.beginPath();
+     context.arc(x, y, radius, 0, (2 * Math.PI), false);
+     context.fill();
 }
 
 /**
   @function getRandomInt(min: number, max: number) -> number
-  @desc Creeren van random geheel getal in het interval [min, max] 
+  @description Creeren van random geheel getal in het interval [min, max]
   @param {number} min een geheel getal als onderste grenswaarde
   @param {number} max een geheel getal als bovenste grenswaarde (max > min)
   @return {number} een random geheel getal x waarvoor geldt: min <= x <= max
@@ -207,19 +229,21 @@ function getRandomInt(min, max) {
 
 /**
   @function createFoods() -> array met food
-  @desc [Element] array van random verdeelde voedselpartikelen
+  @description [Element] array van random verdeelde voedselpartikelen
   @return [Element] array met food
 */
 function createFoods() {   
    var  i = 0,    
-        food;
+        food,
+        tempFoods = [];
    //we gebruiken een while omdat we, om een arraymethode te gebruiken, eerst een nieuw array zouden moeten creëren (met NUMFOODS elementen)
    while (i < NUMFOODS ) {
      food = createFood(XMIN + getRandomInt(0, xMax), YMIN + getRandomInt(0, yMax));
-     if (!food.collidesWithOneOf(snake.segments) && !food.collidesWithOneOf(foods) ) {
-       foods.push(food);
+     if (!food.collidesWithOneOf(snake.segments) && !food.collidesWithOneOf(tempFoods) ) {
+         tempFoods.push(food);
        i++
      }
-   }  
+   }
+   return tempFoods;
 }
 

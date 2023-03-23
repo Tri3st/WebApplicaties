c190e9764d9f86747177c8a9b17da15d9fb41a42
Martin van Diest <martin.van.diest@gmail.com>
2023-03-23 19:27:18 +0100

    Bericht wordt getoond als je af bent.
    Door winst of verlies. (nog beter testen)

--- martin/js/inloggen.js
+++ martin/js/inloggen.js
@@ -5,20 +5,33 @@
 
 $( document ).ready(() => {
     console.log('READY!');
-    const username = $('#username-input');
-    const password = $('#password-input');
+    const username = $('#username-input').val();
+    const password = $('#password-input').val();
+
+    let  currentLoggedin;
+    const usersArray = JSON.parse(localStorage.getItem("all_users")) || [];
 
     $('#inlogBTN').click(() => {
-
+        if (usersArray.map((user) => user.username).includes(username)){
+            if (usersArray.findIndex((user) => user.username === username && user.password === password)){
+                currentLoggedin = username;
+            }
+        }
     });
     $('#registerBTN').click(() => {
     if (username !== '' && password !== '') {
             const user = {
-                username: username.toString(),
-                password: password.toString()
+                "username": username,
+                "password": password
             };
             console.log(user)
-            localStorage.setItem(`${username}`, `${password}`);
+            if (usersArray.map((user) => user.username).includes(username)){
+                // username already exists
+            }
+            else {
+                usersArray.push({username: username, password: password});
+            }
+            localStorage.setItem('all_users', JSON.stringify(usersArray));
         }
     })
 });
